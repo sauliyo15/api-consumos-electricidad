a2e100ec8ea52b24f2735604dcb7050d16541116
sauliyo15 <gc.saul.7@gmail.com>
2024-12-26 12:07:16 +0100

    INIT Initial commit

---
+++ src/main/java/sauliyo/api_consumos_electricidad/ApiConsumosElectricidadApplication.java
@@ -0,0 +1,13 @@
+package sauliyo.api_consumos_electricidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsumosElectricidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsumosElectricidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sauliyo/api_consumos_electricidad/ApiConsumosElectricidadApplicationTests.java
@@ -0,0 +1,13 @@
+package sauliyo.api_consumos_electricidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumosElectricidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
